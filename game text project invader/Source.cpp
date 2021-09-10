c730a802549d727c9a4af53c3be002032b3d6000
Shidkung <pitpi@DESKTOP-B29REMC>
2021-09-10 13:07:34 +0700

    char cursor(int x, int y) {  HANDLE hStd = GetStdHandle(STD_OUTPUT_HANDLE);  char buf[2]; COORD c = { x,y }; DWORD num_read;  if (   !ReadConsoleOutputCharacter(hStd, (LPTSTR)buf, 1, c, (LPDWORD)&num_read))
      return '\0';
     else
      return buf[0];
    }

--- game text project invader/Source.cpp
+++ game text project invader/Source.cpp
@@ -1,6 +1,18 @@
+
 #include<stdio.h>
 #include<windows.h>
 #include<conio.h>
+#include<time.h>
+char cursor(int x, int y) {
+	HANDLE hStd = GetStdHandle(STD_OUTPUT_HANDLE);
+	char buf[2]; COORD c = { x,y }; DWORD num_read;
+	if (
+		!ReadConsoleOutputCharacter(hStd, (LPTSTR)buf, 1, c, (LPDWORD)&num_read))
+
+		return '\0';
+	else
+		return buf[0];
+}
 void setcursor(bool visible)
 {
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -8,6 +20,10 @@ void setcursor(bool visible)
 	lpCursor.bVisible = visible;
 	lpCursor.dwSize = 20;
 	SetConsoleCursorInfo(console, &lpCursor);
+}
+void setcolor(int fg, int bg) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
 }
 void gotoxy(int x, int y)
 {
@@ -17,55 +33,92 @@ void gotoxy(int x, int y)
 void erase_ship(int x, int y)
 {
 	gotoxy(x, y);
-	printf("           ");
+	setcolor(0, 0);
+	printf("       ");
 }
 void draw_ship(int x, int y)
 {
 	gotoxy(x, y);
-	printf(" (<[|o|]>) ");
+	setcolor(2, 4);
+	printf("<[|o|]>");
 }
 void erase_enemies(int x, int y)
 {
-	gotoxy(x, y);  
-	printf("              ");
+	gotoxy(x, y);
+
+	printf("       ");
 }
 void draw_enemies(int x, int y)
 {
 	gotoxy(x, y);
-	printf(" ().(0)(0).() ");
+	printf(" ((0o0)) ");
 }
 void draw_bullet(int x, int y)
 {
-	gotoxy(x, y); printf("  ^  ");
+	gotoxy(x, y);
+	setcolor(15, 0);
+	printf("^");
 }
 void clear_bullet(int x, int y)
 {
-	gotoxy(x, y); printf("      ");
+	gotoxy(x, y);
+	setcolor(0, 0);
+	printf(" ");
 }
+bool bullet[5];
+int bx[5], by[5], i;
+int bulletnow = 0;
+int direction = 0;
 int main()
 {
+
 	char ch = ' ';
 	int x = 40, y = 15;
-	int bx, by, i;
-	int bullet = 0;
+
 	setcursor(0);
 	draw_ship(x, y);
 	do {
 		if (_kbhit()) {
 			ch = _getch();
 
-			if (ch == 'a' && x > 0) { draw_ship(--x, y); }
-			if (ch == 'd' && x < 80) { draw_ship(++x, y); }
-			if (ch == 'w' && y > 0) { draw_ship(x, --y); erase_ship(x, ++y); draw_ship(x, --y); }
-			if (ch == 's' && y < 30) { draw_ship(x, ++y); erase_ship(x, --y); draw_ship(x, ++y); }
-			if (bullet != 1 && ch == 'f') { bullet = 1; bx = x + 3; by = y - 1; }
+			/*if (ch == 'a' && x > 0) { erase_ship(x, y); draw_ship(--x, y); }
+			if (ch == 'd' && x < 80) {erase_ship(x, y); draw_ship(++x, y); }
+			if (ch == 'w' && y > 0) { erase_ship(x, y); draw_ship(x, --y); }
+			if (ch == 's' && y < 30) { erase_ship(x, y); draw_ship(x, ++y); }*/
+			if (ch == 'a') { direction = -1; }
+			if (ch == 'd') { direction = 1; }
+			if (ch == 's') { direction = 0; }
+			if (bullet[bulletnow] != 1 && ch == ' ') {
+				bullet[bulletnow] = 1;
+				bx[bulletnow] = x + 3;
+				by[bulletnow] = y - 1;
+				bulletnow++;
+				bulletnow %= 5;
+			}
+
 			fflush(stdin);
 		}
-		if (bullet == 1) {
-             clear_bullet(bx, by);
-			if (by == 2) { bullet = 0; }
-			else { draw_bullet(bx, --by); }
+		erase_ship(x, y);
+		x += direction;
+		if (x < 0)
+			x = 0;
+		if (x > 80)
+			x = 80;
+		draw_ship(x, y);
 
+
+
+		for (i = 0; i < 5; i++) {
+			if (bullet[i] == 1) {
+				clear_bullet(bx[i], by[i]);
+				if (by[i] > 0) {
+					draw_bullet(bx[i], --by[i]);
+				}
+				else {
+					bullet[i] == 0;
+				}
+
+			}
 		}
 		Sleep(100);
 	} while (ch != 'x');
